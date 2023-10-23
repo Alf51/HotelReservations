d08f10334873601b2c65ce2c6294c2ca539f0f1f
Vasiliy Vozniuk <zakupx51@yandex.ru>
2023-10-23 21:39:37 +0300

    init commit

---
+++ src/main/java/org/goldenalf/privatepr/PrivatePrApplication.java
@@ -0,0 +1,13 @@
+package org.goldenalf.privatepr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivatePrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivatePrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/goldenalf/privatepr/PrivatePrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.goldenalf.privatepr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivatePrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
