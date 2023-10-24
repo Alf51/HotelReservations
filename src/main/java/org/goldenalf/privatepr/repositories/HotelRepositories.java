ad100f3fd6833340f0b12c90ac446bee54f140d3
Vasiliy Vozniuk <zakupx51@yandex.ru>
2023-10-25 00:36:28 +0300

    upd repository hotel

--- src/main/java/org/goldenalf/privatepr/repositories/HotelRepositories.java
+++ src/main/java/org/goldenalf/privatepr/repositories/HotelRepositories.java
@@ -2,6 +2,8 @@ package org.goldenalf.privatepr.repositories;
 
 import org.goldenalf.privatepr.models.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HotelRepositories extends JpaRepository<Hotel, Integer> {
 }
