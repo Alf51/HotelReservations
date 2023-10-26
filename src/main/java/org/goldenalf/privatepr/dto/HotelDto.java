c6d19c18816c48aa2025640f639f4727c40dc21b
Vasiliy Vozniuk <zakupx51@yandex.ru>
2023-10-26 20:15:36 +0300

    changed record on calss for dto hotel

--- src/main/java/org/goldenalf/privatepr/dto/HotelDto.java
+++ src/main/java/org/goldenalf/privatepr/dto/HotelDto.java
@@ -1,10 +1,57 @@
 package org.goldenalf.privatepr.dto;
 
 
-public record HotelDto(String name,
-                       String address,
-                       int rating,
-                       String description
-) {
+import jakarta.persistence.Column;
+
+public class HotelDto {
+    private String name;
+
+    private String address;
+
+    private int rating;
+
+    private String description;
+
+    public HotelDto() {
+    }
+
+    public HotelDto(String name, String address, int rating, String description) {
+        this.name = name;
+        this.address = address;
+        this.rating = rating;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
 
