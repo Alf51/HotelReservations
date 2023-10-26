7f74d078f105ad07413757890f2dc697cde40d83
Vasiliy Vozniuk <zakupx51@yandex.ru>
2023-10-26 19:29:09 +0300

    create hotel_service

---
+++ src/main/java/org/goldenalf/privatepr/services/HotelService.java
@@ -0,0 +1,42 @@
+package org.goldenalf.privatepr.services;
+
+import lombok.RequiredArgsConstructor;
+import org.goldenalf.privatepr.models.Hotel;
+import org.goldenalf.privatepr.repositories.HotelRepositories;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class HotelService {
+    private final HotelRepositories hotelRepositories;
+
+    @Transactional
+    public void save(Hotel hotel) {
+        hotelRepositories.save(hotel);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        hotelRepositories.deleteById(id);
+    }
+
+    @Transactional
+    public void update(int id, Hotel hotelByUpdate) {
+        hotelByUpdate.setId(id);
+        hotelRepositories.save(hotelByUpdate);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Hotel> getHotel(int id) {
+        return hotelRepositories.findById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Hotel> getAllHotels() {
+        return hotelRepositories.findAll();
+    }
+}
