3eed14c0e8f7d963be4c4b61fc7bbd6a66d4aa69
Vasiliy Vozniuk <zakupx51@yandex.ru>
2023-10-27 15:57:10 +0300

    upd hotel dto

--- src/main/java/org/goldenalf/privatepr/dto/HotelDto.java
+++ src/main/java/org/goldenalf/privatepr/dto/HotelDto.java
@@ -1,69 +1,37 @@
 package org.goldenalf.privatepr.dto;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.goldenalf.privatepr.models.Room;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class HotelDto {
-    @Column(name = "name")
     @NotEmpty(message = "имя отеля не может быть пустым")
     @NotNull(message = "введите имя отеля")
     private String name;
 
-    @Column(name = "address")
     @NotEmpty(message = "адрес отеля не может быть пустым")
     @NotNull(message = "введите адрес отеля")
     private String address;
 
-    @Column(name = "rating")
     @Min(value = 0, message = "оценка не может быть ниже 0")
     @Max(value = 5, message = "Отель ценит вашу оценку, но максимальный бал не может превышать 5")
     private int rating;
 
     private String description;
 
-    public HotelDto() {
-    }
-
-    public HotelDto(String name, String address, int rating, String description) {
-        this.name = name;
-        this.address = address;
-        this.rating = rating;
-        this.description = description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    private List<RoomDto> roomList = new ArrayList<>();
 }
 
