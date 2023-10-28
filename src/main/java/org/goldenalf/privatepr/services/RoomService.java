c2921e17f24bae9239c74c2f9deecf46b374e7aa
Vasiliy Vozniuk <zakupx51@yandex.ru>
2023-10-28 16:00:37 +0300

    create room service

---
+++ src/main/java/org/goldenalf/privatepr/services/RoomService.java
@@ -0,0 +1,65 @@
+package org.goldenalf.privatepr.services;
+
+import lombok.RequiredArgsConstructor;
+import org.goldenalf.privatepr.models.Hotel;
+import org.goldenalf.privatepr.models.Room;
+import org.goldenalf.privatepr.repositories.RoomRepositories;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RoomService {
+    private final RoomRepositories roomRepositories;
+    private final HotelService hotelService;
+
+    @Transactional
+    public void save(Room room) {
+        roomRepositories.save(room);
+    }
+
+    @Transactional
+    //Добавить валидацию и в случае чего выкинуть сообщение, что не удалось создать комнату
+    public void save(Room room, int hotelId) {
+        Optional<Hotel> hotel = hotelService.getHotel(hotelId);
+        if (hotel.isPresent()) {
+            room.setHotel(hotel.get());
+            roomRepositories.save(room);
+        }
+    }
+
+    @Transactional
+    public void delete(int id) {
+        roomRepositories.deleteById(id);
+    }
+
+    @Transactional
+    //TODO добавить валидацию
+    public void update(int id, Room roomByUpdate) {
+        Optional<Room> room = roomRepositories.findById(id);
+
+        if (room.isPresent()) {
+            roomByUpdate.setHotel(room.get().getHotel());
+            roomByUpdate.setId(id);
+            roomRepositories.save(roomByUpdate);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Room> getRoom(int id) {
+        return roomRepositories.findById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Room> findAllByHotelId(int hotelId) {
+        return roomRepositories.findAllByHotelId(hotelId);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Room> getAllRooms() {
+        return roomRepositories.findAll();
+    }
+}
