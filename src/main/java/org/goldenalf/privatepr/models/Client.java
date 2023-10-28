c78456edae7ff53fe4c73321b743a43240c9cc00
Vasiliy Vozniuk <zakupx51@yandex.ru>
2023-10-28 20:29:58 +0300

    add client model

---
+++ src/main/java/org/goldenalf/privatepr/models/Client.java
@@ -0,0 +1,41 @@
+package org.goldenalf.privatepr.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity(name = "clients")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "birthdate")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "dd-MM-YYYY")
+    private String birthdate;
+
+    public Client(String name, String login, String password, String birthdate) {
+        this.name = name;
+        this.login = login;
+        this.password = password;
+        this.birthdate = birthdate;
+    }
+}
--- src/main/java/org/goldenalf/privatepr/models/Hotel.java
+++ src/main/java/org/goldenalf/privatepr/models/Hotel.java
@@ -9,8 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
+
 
 import java.util.ArrayList;
 import java.util.List;
