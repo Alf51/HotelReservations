77f71f9a91ca896251bf3263624a4b6bbb845e51
Vasiliy Vozniuk <zakupx51@yandex.ru>
2023-10-30 00:06:31 +0300

    создал репу review

---
+++ src/main/java/org/goldenalf/privatepr/repositories/ReviewRepository.java
@@ -0,0 +1,11 @@
+package org.goldenalf.privatepr.repositories;
+
+import org.goldenalf.privatepr.models.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+}
