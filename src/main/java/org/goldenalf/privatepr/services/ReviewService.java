89579fadd51223c504164830816125c605382ad5
Vasiliy Vozniuk <zakupx51@yandex.ru>
2023-10-30 00:06:52 +0300

    создал сервис review

---
+++ src/main/java/org/goldenalf/privatepr/services/ReviewService.java
@@ -0,0 +1,42 @@
+package org.goldenalf.privatepr.services;
+
+import lombok.RequiredArgsConstructor;
+import org.goldenalf.privatepr.models.Review;
+import org.goldenalf.privatepr.repositories.ReviewRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+    private final ReviewRepository reviewRepository;
+
+    @Transactional
+    public void save(Review review) {
+        reviewRepository.save(review);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        reviewRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void update(int id, Review reviewByUpdate) {
+        reviewByUpdate.setId(id);
+        reviewRepository.save(reviewByUpdate);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Review> getReview(int id) {
+        return reviewRepository.findById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Review> getAllReview() {
+        return reviewRepository.findAll();
+    }
+}
