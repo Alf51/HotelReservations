3e8f682f795a0aff794d0b47c77cff76e8de3449
Vasiliy Vozniuk <zakupx51@yandex.ru>
2023-10-31 21:52:13 +0300

    добавил класс с ошибкой для book

---
+++ src/main/java/org/goldenalf/privatepr/utils/erorsHandler/bookError/BookErrorException.java
@@ -0,0 +1,7 @@
+package org.goldenalf.privatepr.utils.erorsHandler.bookError;
+
+public class BookErrorException extends RuntimeException {
+    public BookErrorException(String message) {
+        super(message);
+    }
+}
