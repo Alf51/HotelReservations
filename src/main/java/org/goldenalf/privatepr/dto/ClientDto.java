fb979c93cfd5e5df064bb0fc3d7fc5dfc789f8d9
Vasiliy Vozniuk <zakupx51@yandex.ru>
2023-11-08 23:09:41 +0300

    добавил доп. валидацию для полей ClientDto

--- src/main/java/org/goldenalf/privatepr/dto/ClientDto.java
+++ src/main/java/org/goldenalf/privatepr/dto/ClientDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,6 +20,8 @@ import java.time.LocalDate;
 public class ClientDto {
     @NotEmpty(message = "имя не может быть пустым")
     @NotNull(message = "введите имя")
+    @Size(min = 3, max = 50, message = "Имя должно быть от 3х до 50 символов")
+    @Size(min = 3, max = 50, message = "Логин должен содержать 3х до 50 символов")
     private String name;
 
     @NotEmpty(message = "login не может быть пустым")
