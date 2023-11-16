f6a283fd88755ac552025b9caca51a7e7b82d349
Vasiliy Vozniuk <zakupx51@yandex.ru>
2023-11-16 23:58:28 +0300

    добавил вывод сообщений об ошибке для english локали

--- src/main/java/org/goldenalf/privatepr/PrivatePrApplication.java
+++ src/main/java/org/goldenalf/privatepr/PrivatePrApplication.java
@@ -3,6 +3,7 @@ package org.goldenalf.privatepr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PrivatePrApplication {
 
