2130a2890b353bde177c8eeda7d6df5075504979
Vasiliy Vozniuk <zakupx51@yandex.ru>
2023-11-27 21:52:15 +0300

    рефакторинг ErrorControllerAdvice - объединил исключения в одном методе

--- src/main/java/org/goldenalf/privatepr/controllers/ErrorControllerAdvice.java
+++ src/main/java/org/goldenalf/privatepr/controllers/ErrorControllerAdvice.java
@@ -21,62 +21,37 @@ public class ErrorControllerAdvice {
 
     private final ErrorHandler errorHandler;
 
-    @ExceptionHandler(DateTimeParseException.class)
-    private ResponseEntity<ErrorResponse> handleDateTimeParseExceptionException(DateTimeParseException e) {
-        String errorMessage = errorHandler
-                .getErrorMessage("validation.hotelBook.date.exception.message-date") + e.getParsedString();
-
-        ErrorResponse errorResponse = new ErrorResponse(errorMessage, System.currentTimeMillis());
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(HotelErrorException.class)
-    private ResponseEntity<ErrorResponse> handleHotelErrorExceptionException(HotelErrorException e) {
-        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), System.currentTimeMillis());
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(ClientErrorException.class)
-    private ResponseEntity<ErrorResponse> handleClientErrorExceptionException(ClientErrorException e) {
-        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), System.currentTimeMillis());
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(RoomErrorException.class)
-    private ResponseEntity<ErrorResponse> handleRoomErrorExceptionException(RoomErrorException e) {
-        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), System.currentTimeMillis());
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(BookErrorException.class)
-    private ResponseEntity<ErrorResponse> handleBookErrorExceptionException(BookErrorException e) {
+    @ExceptionHandler({HotelErrorException.class,
+            ClientErrorException.class,
+            RoomErrorException.class,
+            BookErrorException.class,
+            InsufficientAccessException.class,
+            ReviewErrorException.class})
+    public ResponseEntity<ErrorResponse> handleHotelErrorExceptionException(Exception e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), System.currentTimeMillis());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    private ResponseEntity<ErrorResponse> handleMethodArgumentNotValidExceptionException(MethodArgumentNotValidException e) {
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidExceptionException(MethodArgumentNotValidException e) {
         String errorMessage = errorHandler.getErrorMessage(e.getBindingResult());
         ErrorResponse errorResponse = new ErrorResponse(errorMessage, System.currentTimeMillis());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(InsufficientAccessException.class)
-    private ResponseEntity<ErrorResponse> handleInsufficientAccessExceptionException(InsufficientAccessException e) {
-        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), System.currentTimeMillis());
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(ReviewErrorException.class)
-    private ResponseEntity<ErrorResponse> handleReviewErrorExceptionException(ReviewErrorException e) {
-        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), System.currentTimeMillis());
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
-
     @ExceptionHandler(Exception.class)
-    private ResponseEntity<ErrorResponse> handleException(Exception e) {
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
         logger.error(e.getMessage());
         String errorMessage = errorHandler.getErrorMessage("validation.hotelBook.server.error");
+        ErrorResponse errorResponse = new ErrorResponse(errorMessage, System.currentTimeMillis());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DateTimeParseException.class)
+    public ResponseEntity<ErrorResponse> handleDateTimeParseExceptionException(DateTimeParseException e) {
+        String errorMessage = errorHandler
+                .getErrorMessage("validation.hotelBook.date.exception.message-date") + e.getParsedString();
+
         ErrorResponse errorResponse = new ErrorResponse(errorMessage, System.currentTimeMillis());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
