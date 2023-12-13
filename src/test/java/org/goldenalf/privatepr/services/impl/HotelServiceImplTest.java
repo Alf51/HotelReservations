a1ea71888a5bd694e3e6d3a4e180f206e748952b
Vasiliy Vozniuk <zakupx51@yandex.ru>
2023-12-14 00:51:04 +0300

    добавил тесты для HotelServiceImplTest

--- src/test/java/org/goldenalf/privatepr/services/impl/HotelServiceImplTest.java
+++ src/test/java/org/goldenalf/privatepr/services/impl/HotelServiceImplTest.java
@@ -4,6 +4,7 @@ import org.goldenalf.privatepr.models.Hotel;
 import org.goldenalf.privatepr.models.Room;
 import org.goldenalf.privatepr.repositories.HotelRepository;
 import org.goldenalf.privatepr.utils.erorsHandler.ErrorHandler;
+import org.goldenalf.privatepr.utils.exeptions.HotelErrorException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,18 +12,57 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class HotelServiceImplTest {
     @Mock
     private HotelRepository hotelRepository;
-    @Mock
-    private ErrorHandler errorHandler;
     @InjectMocks
     private HotelServiceImpl hotelService;
+
+    @Test
+    void delete_forNonExistHotel_deleteFail() {
+        int nonexistentHotelId = 2;
+
+        when(hotelService.getHotel(nonexistentHotelId)).thenReturn(Optional.empty());
+
+        // Проверяем, что при удалении отсутствующего отеля выбрасывается исключение HotelErrorException
+        assertThrows(HotelErrorException.class, () -> hotelService.delete(nonexistentHotelId));
+
+        // Проверяем, что hotelRepository.deleteById не был вызван
+        verify(hotelRepository, never()).deleteById(nonexistentHotelId);
+    }
+
+    @Test
+    void delete_forExistHotel_deleteSuccess() {
+        Hotel hotel = getHotel();
+        int hotelId = hotel.getId();
+
+        when(hotelService.getHotel(hotelId)).thenReturn(Optional.of(hotel));
+
+        hotelService.delete(hotelId);
+
+        // Проверяем, что hotelRepository.deleteById был вызван с правильным аргументом
+        verify(hotelRepository).deleteById(hotelId);
+    }
+
+    @Test
+    void update_updateHotelSuccess() {
+        // Создаем тестируемые данные
+        Hotel hotelByUpdate = getHotel();
+        int hotelId = hotelByUpdate.getId();
+
+        // Вызываем метод update
+        hotelService.update(hotelId, hotelByUpdate);
+
+        // Проверяем, что hotelRepository.save был вызван с правильным аргументом
+        verify(hotelRepository).save(hotelByUpdate);
+    }
 
     @Test
     void save_saveHotelSuccess() {
